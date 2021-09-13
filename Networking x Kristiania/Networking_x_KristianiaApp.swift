6979c7f1939d377077d96e28cb2a339b67c63b71
Konstantin Loginov <loginov.k@gmail.com>
2021-09-13 18:29:23 +0200

    Initial Commit

---
+++ Networking x Kristiania/Networking_x_KristianiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_x_KristianiaApp.swift
+//  Networking x Kristiania
+//
+//  Created by Konstantin Loginov on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Networking_x_KristianiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
